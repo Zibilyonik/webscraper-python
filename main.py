d5cfe73edc7748f928b9493cc2783e56ddafbe2f
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-04-01 21:36:49 +0200

    added scraper for page title and page description sections

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 # python3
-import requests
 
+import requests
+from bs4 import BeautifulSoup
 
 def main():
-    print("Input the URL:")
-    link = input()
-    response = requests.get(link)
+    web_dict={}
+    while True:
+        link = input("Input the URL:")
+        if link.find("nature.com/articles") != -1:
+            break
+        print("Invalid page!")
     try:
-        print(response.json()['content'])
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, "html.parser")
+        if soup.find("title").text:
+            web_dict["title"] = soup.find("title").text
+            web_dict["description"] = soup.findAll("meta", {"name": "description"})[0]["content"]
     except KeyError:
         print("Invalid quote resource")
+    else:
+        print(web_dict)
 
 
 main()
