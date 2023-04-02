82f9da2efcc52b86f242fbda87b12d5a4af4a528
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-04-02 21:10:25 +0200

    changed code functionality to saving a page html code to file

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 # python3
 
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+
 
 def main():
-    web_dict={}
-    while True:
-        link = input("Input the URL:")
-        if link.find("nature.com/articles") != -1:
-            break
-        print("Invalid page!")
-    try:
-        response = requests.get(link)
-        soup = BeautifulSoup(response.text, "html.parser")
-        if soup.find("title").text:
-            web_dict["title"] = soup.find("title").text
-            web_dict["description"] = soup.findAll("meta", {"name": "description"})[0]["content"]
-    except KeyError:
-        print("Invalid quote resource")
+    print("Input the URL:")
+    link = input()
+    response = requests.get(link)
+    if response:
+        file = open('source.html', 'wb')
+        file.write(response.content)
+        file.close()
+        print("Content saved.")
     else:
-        print(web_dict)
+        print("The URL returned %s!" % response.status_code)
 
 
 main()
