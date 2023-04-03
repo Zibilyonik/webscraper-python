420553b1d945b6fbbc4503e3b6477af27eaf4cd0
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-04-03 17:12:19 +0200

    added option to select type of article to scrape, and from how many pages

--- main.py
+++ main.py
@@ -3,20 +3,18 @@
 import requests
 from bs4 import BeautifulSoup
 import string
+import os
 
-page = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'
 
-
-def get_data():
-    response = requests.get(page)
+def get_data(page, article_type):
+    response = requests.get('https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=' + str(page))
     items = {}
     soup = BeautifulSoup(response.text, 'html.parser')
     articles = soup.find_all('article')
     for article in articles:
         title = article.find('span', class_='c-meta__type').text
-        body_link= article.find('div', class_='c-card__link u-link-inherit').text
-        testing = article
-        if title == 'News':
+        body_link= article.find('a', class_='c-card__link u-link-inherit')['href']
+        if title == str(article_type):
             title = article.find('a', class_='c-card__link').text
             for i in title:
                 if i in string.punctuation:
@@ -24,20 +22,29 @@ def get_data():
                 elif i in string.whitespace:
                     title = title.replace(i, '_')
             items[title] = body_link
-    print(testing)
     return items
 
-def write_data(items):
+def write_data(page, items):
+    os.mkdir(f'Page_{page}')
+    os.chdir(f'Page_{page}')
     for item in items:
         response = requests.get(f'https://www.nature.com{items[item]}')
         soup = BeautifulSoup(response.text, 'html.parser')
-        body = soup.find('p', class_='article__teaser')
+        body = soup.find('p', class_='article__teaser').text
         file = open(f'{str(item)}.txt', 'wb')
         file.write(str(body).encode())
         file.close()
+    os.chdir('..')
+
+#this function loops through the pages and calls the get_data function
+def get_pages(pages, article_type):
+    for i in range(1, int(pages)+1):
+        write_data(i, get_data(i, article_type))
 
 def main():
-    write_data(get_data())
+    pages = input()
+    article_type = input()
+    get_pages(int(pages), article_type)
 
 
 main()
